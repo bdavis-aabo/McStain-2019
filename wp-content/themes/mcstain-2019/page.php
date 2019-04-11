671c3166b7b7ad35243c4af21559e683f3f37504
Bryan <bdavis@artandbusinessone.com>
2019-04-11 13:47:19 -0600

    Thank you page default template

---
+++ wp-content/themes/mcstain-2019/page.php
@@ -0,0 +1,27 @@
+<?php // Default Page Template ?>
+
+<?php get_header() ?>
+
+  <section class="manifesto-heroimage homepage-heroimage">
+    <?php echo get_the_post_thumbnail($post->ID, 'full', array('class' => 'img-fluid aligncenter')); ?>
+    <div class="homepage-heroimage-caption">
+      <h1 class="caption-title">Thank You</h1>
+      <!-- <p class="caption-content">Subheadline Here.</p> -->
+    </div>
+  </section>
+
+  <section class="homepage-content homepage-section manifesto-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-md-11">
+          <div class="homepage-leftcolumn">
+            <?php while(have_posts()): the_post() ?>
+              <?php the_content() ?>
+            <?php endwhile; ?>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+<?php get_footer() ?>
