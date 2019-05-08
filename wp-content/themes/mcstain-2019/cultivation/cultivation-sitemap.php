9d88e06d6f8023c363b765095b931bbb97cc0aa1
Bryan <bdavis@artandbusinessone.com>
2019-05-08 12:32:49 -0600

    Show button on mobile for site plan map

--- wp-content/themes/mcstain-2019/cultivation/cultivation-sitemap.php
+++ wp-content/themes/mcstain-2019/cultivation/cultivation-sitemap.php
@@ -5,6 +5,10 @@
         <div class="sitemap-iframe">
           <iframe src="<?php echo get_field('cultivation_map_url') ?>" width="100%"></iframe>
         </div>
+
+        <div class="sitemap-button">
+          <a href="<?php echo get_field('cultivation_map_url') ?>" class="green-btn btn" target="_blank"><i class="fas fa-chevron-right"></i><i class="fas fa-chevron-right"></i> View Community Site Plan</a>
+        </div>
       </div>
     </div>
   </div>
