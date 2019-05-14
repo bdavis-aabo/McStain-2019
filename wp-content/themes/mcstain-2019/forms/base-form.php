3edd212b5da3d09c0b6c6cbda171a603ad07b545
Bryan <bdavis@artandbusinessone.com>
2019-05-14 15:58:45 -0600

    Adding generic project id as default if no community is present on base form.

--- wp-content/themes/mcstain-2019/forms/base-form.php
+++ wp-content/themes/mcstain-2019/forms/base-form.php
@@ -28,7 +28,7 @@
 
   <input type="hidden" name="LassoUID" value="zFht#iXi[2" />
   <input type="hidden" name="ClientID" value="1591" />
-  <input type="hidden" name="ProjectID" value="<?php echo $_projectID ?>" />
+  <input type="hidden" name="ProjectID" value="<?php if($_projectID != ''): echo $_projectID; else: echo '10711'; endif; ?>" />
 
   <?php if($_communityUrl != ''): ?>
     <input type="hidden" name="SignupThankyouLink" value="<?php bloginfo('url') ?>/contact-us/thank-you?community=<?php echo $_communityUrl ?>" />
