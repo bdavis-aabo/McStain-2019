b90967fac32e9cd160e1c1d8f5faa934389f9c8f
Bryan <bdavis@artandbusinessone.com>
2019-06-28 14:36:47 -0600

    Update to button on CCF page to gallery

--- wp-content/themes/mcstain-2019/cultivation/cultivation-models_new.php
+++ wp-content/themes/mcstain-2019/cultivation/cultivation-models_new.php
@@ -77,7 +77,7 @@ $_floorplans->query($_args);
 
         <div class="col-6 col-sm-4 col-md-4 col-lg-2 right-btn">
           <article class="model">
-            <button class="gold-btn btn btn-block model-btn" onclick="window.location.href = '/communities/<?php echo $post->post_name ?>/<?php echo $post->post_name ?>-photo-gallery';" target="_blank">
+            <button class="gold-btn btn btn-block model-btn" onclick="window.location.href = '/communities/<?php echo $post->post_name ?>/<?php echo $post->post_name ?>-gallery';" target="_blank">
               <span class="arrows"><i class="fa fa-chevron-right"></i><i class="fa fa-chevron-right"></i><i class="fa fa-chevron-right"></i></span>
               <span class="text"> See More</span>
             </button>
@@ -88,6 +88,6 @@ $_floorplans->query($_args);
   </div>
   <!-- /end normal models -->
 
-  <button class="btn gold-btn sidebar-btn model-btn cult-trigger"><i class="fa fa-chevron-right"></i><i class="fa fa-chevron-right"></i> See more</button>
+  <button class="btn gold-btn sidebar-btn model-btn" onclick="window.location.href = '/communities/<?php echo $post->post_name ?>/<?php echo $post->post_name ?>-gallery';" target="_blank"><i class="fa fa-chevron-right"></i><i class="fa fa-chevron-right"></i> See more</button>
 </section>
 <?php endif; ?>
