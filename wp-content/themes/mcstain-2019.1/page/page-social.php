69eadf76dacd5ad1523287b951edd3fd0f2c1c9b
Bryan <bdavis@artandbusinessone.com>
2020-03-27 09:06:45 -0600

    Update FB url to new profile page

---
+++ wp-content/themes/mcstain-2019.1/page/page-social.php
@@ -0,0 +1,6 @@
+            <ul class="footer-social">
+              <li><a href="https://www.facebook.com/McStainNeighborhoods/" target="_blank"><i class="fab fa-facebook"></i></a></li>
+              <li><a href="https://www.instagram.com/mcstainneighborhoods/" target="_blank"><i class="fab fa-instagram"></i></a></li>
+              <li><a href="https://www.linkedin.com/company/56154" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+              <li><a href="https://www.youtube.com/user/McStainNeighborhoods" target="_blank"><i class="fab fa-youtube"></i></a></li>
+            </ul>
