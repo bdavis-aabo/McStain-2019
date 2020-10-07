2e1d0cb6b5d8bf2b8ccccd87fe22a9e027997f48
Bryan <bdavis@artandbusinessone.com>
2020-10-07 13:43:07 -0600

    resaving css file and form labels.

--- wp-content/themes/mcstain-2019.1/forms/base-form.php
+++ wp-content/themes/mcstain-2019.1/forms/base-form.php
@@ -5,7 +5,7 @@
 
 <div class="email-form base-contactform">
   <h1 class="aqua-txt"><?php the_title() ?></h1>
-  <img src="<?php bloginfo('stylesheet_directory') ?>/assets/images/leaf.png" class="alignleft" />
+  <img src="<?php bloginfo('stylesheet_directory') ?>/assets/images/leaf.png" alt="<?php bloginfo('name') ?> - Leaf Logo" class="alignleft" />
   <?php if(!is_page('quick-move-in-homes')): ?>
     <p>I want to be the first to know about McStain at <?php echo $_community ?>.<br />Please keep me informed&hellip;and excited.</p>
   <?php elseif(is_page('quick-move-in-homes')): ?>
