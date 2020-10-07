9e599a0121c6206bee1dc3d46b97320d4097b4c5
Bryan <bdavis@artandbusinessone.com>
2020-10-07 14:02:12 -0600

    Update to manifesto section to include correct contact form.

---
+++ wp-content/themes/mcstain-2019.1/manifesto/manifesto-lightbox.php
@@ -0,0 +1,43 @@
+<?php
+$_manifestoSlides = new WP_Query();
+$_args = array(
+  'post_type' => 'manifesto_messages',
+  'post_status' => 'publish',
+  'posts_per_page' => -1,
+  'order' => 'asc',
+  'orderby' => 'menu_order'
+);
+$_manifestoSlides->query($_args);
+?>
+
+<?php if($_manifestoSlides->have_posts()): ?>
+<section class="email-section members-mask manifesto-lightbox">
+  <div class="email-section-header">
+    <a class="close close-btn"><i class="fa fa-times"></i></a>
+  </div>
+  <?php while($_manifestoSlides->have_posts()): $_manifestoSlides->the_post() ?>
+  <div class="manifesto-box" id="<?php echo $post->post_name ?>">
+    <div class="manifesto-content <?php echo $post->post_name.'-contents' ?>">
+    <?php if($post->post_name == 'excite'):     //links to communities ?>
+      <h1 class="blue-txt"><?php echo get_field('manifesto_lightbox_title') ?></h1>
+      <?php get_template_part('manifesto/manifesto-excite') ?>
+    <?php elseif($post->post_name == 'place'):  //communities list ?>
+      <h1 class="ltgreen-txt"><?php echo get_field('manifesto_lightbox_title') ?></h1>
+      <?php get_template_part('manifesto/manifesto-place') ?>
+    <?php elseif($post->post_name == 'right'):  //testimonials ?>
+      <h1 class="ltgreen-txt"><?php echo get_field('manifesto_lightbox_title') ?></h1>
+      <?php get_template_part('manifesto/manifesto-right') ?>
+    <?php elseif($post->post_name == 'home'):   //map ?>
+      <h1 class="ltgreen-txt"><?php echo get_field('manifesto_lightbox_title') ?></h1>
+      <?php echo do_shortcode('[wpgmza id="1"]');
+    else:
+    ?>
+      <h1 class="ltgreen-txt"><?php echo get_field('manifesto_lightbox_title') ?></h1>
+      <?php get_template_part('manifesto/manifesto-contact'); ?>
+    <?php endif; ?>
+    </div>
+
+  </div>
+  <?php endwhile; ?>
+</section>
+<?php endif; wp_reset_query() ?>
