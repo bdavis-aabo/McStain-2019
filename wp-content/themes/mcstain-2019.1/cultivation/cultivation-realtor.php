d84bf15db75e2c0851f573a99fb64e2a0adfe2ca
Bryan <bdavis@artandbusinessone.com>
2020-10-09 09:08:43 -0600

    Update to contact form code, to pull the correct forms.

--- wp-content/themes/mcstain-2019.1/cultivation/cultivation-realtor.php
+++ wp-content/themes/mcstain-2019.1/cultivation/cultivation-realtor.php
@@ -4,9 +4,11 @@
   </div>
 
   <div class="email-section-content">
+    <?php if(!is_front_page() && !is_page('home')): ?>
     <div class="email-content base-form form">
       <?php get_template_part('forms/base-form') ?>
     </div>
+    <?php endif; ?>
     <div class="email-content realtor-form form">
       <?php get_template_part('forms/realtor-form') ?>
     </div>
