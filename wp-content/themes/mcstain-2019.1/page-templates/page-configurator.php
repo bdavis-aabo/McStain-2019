4dac7d9ad0d468422789c31856c169f2e3731185
Bryan <bdavis@artandbusinessone.com>
2020-12-03 16:06:38 -0700

    Update to floorplan configurator file.

--- wp-content/themes/mcstain-2019.1/page-templates/page-configurator.php
+++ wp-content/themes/mcstain-2019.1/page-templates/page-configurator.php
@@ -3,7 +3,7 @@
 
 <section class="configurator-container">
   <div class="configurator-frame-container">
-    <iframe id="configurator" src="" frameborder="0" data-pathname=""></iframe>
+    <iframe id="configurator" src="" allowfullscreen frameborder="0" height="100%" width="100%" data-pathname="" onLoad="onLoadIframe()"></iframe>
   </div>
 </section>
 
@@ -13,57 +13,192 @@
 
 
 <script type="text/javascript">
-  var INITIAL = "/project/paintedprairie-4510" /* This is the default link to point the user to, when no hash is specified */
-  var DOMAIN = "https://configurator.creatomus.com";
-  var iframe = document.querySelector("iframe#configurator");
-  var initialLocation = window.location.hash.slice(1);
+    var INITIAL = "/project/paintedprairie-4510" /* This is the default link to point the user to, when no hash is specified */
 
-  if (!initialLocation) {
-    initialLocation = INITIAL;
-  }
-  function listener(event) {
-    if (event.origin != DOMAIN) {
-      return;
+    var DOMAIN = "https://configurator.creatomus.com";
+
+    var USER_TOKEN = '';
+
+     
+
+    var iframe = window.configurator;
+
+    var initialLocation = window.location.hash.slice(1);
+
+ 
+
+    if (!initialLocation) {
+
+      initialLocation = INITIAL;
+
     }
-    var data = parse(event.data);
-    if (data === null) {
-      return;
+
+ 
+
+    function listener(event) {
+
+      if (event.origin != DOMAIN) {
+
+        return;
+
+      }
+
+ 
+
+      var data = typeof event.data === 'string' ? parse(event.data) : event.data;
+
+ 
+
+      if (data === null && data.messageType !== 'CHANGE_LOCATION') {
+
+        return;
+
+      }
+
+ 
+
+      if (window.location.hash === "#" + data.pathname + data.search) {
+
+        return;
+
+      }
+
+ 
+
+      setNewLocation(data);
+
     }
-    if (window.location.hash === "#" + data.pathname + data.search) {
-      return;
+
+ 
+
+    function parse(raw) {
+
+      try {
+
+        var data = JSON.parse(raw);
+
+  
+
+        return data;
+
+      } catch (error) {
+
+        console.error("Bad format of the message");
+
+  
+
+        return null;
+
+      }
+
     }
-    setNewLocation(data);
-  }
-  function parse(raw) {
-    try {
-      var data = JSON.parse(raw);
-      return data;
-    } catch (error) {
-      console.error("Bad format of the message");
-      return null;
+
+ 
+
+    function setNewLocation(data) {
+
+      var newLocation = "#" + data.pathname + data.search;
+
+ 
+
+      window.history.replaceState(data, "", newLocation);
+
+ 
+
+      setIframeDataset(data.pathname, data.search);
+
     }
-  }
-  function setNewLocation(data) {
-    var newLocation = "#" + data.pathname + data.search;
-    window.history.replaceState(data, "", newLocation);
-    iframe.dataset.pathname = data.pathname;
-  }
 
-  function hashChangeListener(event) {
-    var newLocation = window.location.hash.slice(1);
-    setIframeSrc(newLocation);
-  }
+ 
 
-  function setIframeSrc(location) {
-    iframe.setAttribute("src", DOMAIN + location);
-    iframe.dataset.pathname = location;
-  }
+    function hashChangeListener(event) {
 
-  window.addEventListener("message", listener);
-  window.addEventListener("hashchange", hashChangeListener);
+      var newLocation = window.location.hash.slice(1);
 
-  setIframeSrc(initialLocation);
-</script>
+ 
+
+      setIframeSrc(newLocation);
+
+    }
+
+ 
+
+    function setIframeSrc(location, userToken) {
+
+      var arr = location.split('?');
+
+ 
+
+      var pathname = arr[0];
+
+      var search = arr[1] ? '?' + arr[1] : '';
+
+ 
+
+      if (userToken) {
+
+        search = search + (search ? '&' : '?') + 'user_token=' + userToken
+
+      }
+
+ 
+
+      iframe.setAttribute("src", DOMAIN + pathname + search);
+
+ 
+
+      setIframeDataset(pathname, search);
+
+    }
+
+ 
+
+    function setIframeDataset(pathname, search) {
+
+      iframe.dataset.pathname = pathname;
+
+      iframe.dataset.search = search;
+
+    }
+
+ 
+
+    window.addEventListener("message", listener);
+
+    window.addEventListener("hashchange", hashChangeListener);
+
+ 
+
+    setIframeSrc(initialLocation, USER_TOKEN);
+
+ 
+
+    function onLoadIframe() {
+
+      var data = {
+
+        messageType: 'SET_PARENT_LOCATION',
+
+        origin: window.location.origin,
+
+        pathname: window.location.pathname,
+
+        hash: window.location.hash
+
+      };
+
+ 
+
+      setTimeout(function() {
+
+        iframe.contentWindow.postMessage(data, DOMAIN);
+
+      }, 2000);
+
+    }
+
+  </script>
+
 
 </body>
 </html>
