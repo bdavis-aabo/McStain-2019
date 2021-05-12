39f6d57328701deb1a6164ea4be88347f76516a5
Bryan <bdavis@artandbusinessone.com>
2021-05-11 18:06:35 -0600

    another fix

--- wp-content/themes/mcstain-2019.1/cultivation/single-floorplans.php
+++ wp-content/themes/mcstain-2019.1/cultivation/single-floorplans.php
@@ -39,7 +39,7 @@
             <p class="floorplan-title"><?php the_title() ?></p>
             <?php if(have_rows('floorplan_details')): the_row(); ?>
               <p class="details">
-                <?php echo get_sub_field('square_footage') ?> sq ft finished above ground<br/>
+                <?php echo get_sub_field('square_footage') ?> sq ft finished<br/>
                 <?php echo get_sub_field('bedrooms') ?> beds | <?php echo get_sub_field('bathrooms') ?> baths
               </p>
               <p class="price">From $<?php echo get_sub_field('starting_price') ?></p>
