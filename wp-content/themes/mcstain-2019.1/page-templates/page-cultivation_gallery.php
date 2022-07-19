4153de258f6ac0e7eabec8b29f0fb8858ec15ef9
bdavis-aabo <bdavis@artandbusinessone.com>
2022-07-19 07:03:55 -0600

    Update to gallery page template

--- wp-content/themes/mcstain-2019.1/page-templates/page-cultivation_gallery.php
+++ wp-content/themes/mcstain-2019.1/page-templates/page-cultivation_gallery.php
@@ -14,7 +14,7 @@
           </div>
           <div class="col-md-3 col-lg-3">&nbsp;</div>
         </div>
-        <?php if(!is_page('painted-prairie-gallery') && !is_page('showcase-model-gallery') && !is_page('limelight-model-gallery')): ?>
+        <?php if(!is_page('painted-prairie-gallery') && !is_page('showcase-model-gallery') && !is_page('limelight-model-gallery') && !is_page('arras-park-gallery')): ?>
         <div class="row">
           <div class="col-12">
             <?php if(do_shortcode('[envira-gallery slug="'.$post->post_name.'"]') != ''):
