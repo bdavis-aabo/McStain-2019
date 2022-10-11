b815dd2c81830ccfca164b986b0c6387ab265073
bdavis-aabo <bdavis@artandbusinessone.com>
2022-10-11 11:41:25 -0600

    Update to homepage heroimage stylesheet and code

--- wp-content/themes/mcstain-2019.1/home/homepage-hero.php
+++ wp-content/themes/mcstain-2019.1/home/homepage-hero.php
@@ -1,8 +1,8 @@
-  <section class="homepage-section homepage-heroimage">
-    <?php echo get_the_post_thumbnail($post->ID, 'full', array('class' => 'img-fluid aligncenter')); ?>
+<?php $_heroimage = get_the_post_thumbnail_url(get_the_ID(),'full'); ?>
+  <section class="homepage-section homepage-heroimage" style="background: url(<?php echo $_heroimage; ?>) no-repeat center top;">
     <div class="homepage-heroimage-caption">
-      <h1 class="caption-title" style="font-size: 5em;">BeWell House<sup style="font-size: .25em; top: -2.25em;">SM</sup>.</h1>
-      <h2 class="caption-content" style="font-size: 2em;">Your well-being is built in for life.</h2>
+      <h1 class="caption-title">BeWell House<sup style="font-size: .25em; top: -2.25em;">SM</sup>.</h1>
+      <h2 class="caption-content">Your well-being is built in for life.</h2>
 
       <a href="/bewell-house-by-mcstain/" class="green-btn btn">Learn More</a>
     </div>
